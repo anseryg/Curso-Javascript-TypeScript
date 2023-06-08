d06d5199d49ed5adc53dc65add57a9444fa65cc1
dreynus <72932055+dreynus@users.noreply.github.com>
2023-06-08 18:15:02 -0300

    more things

--- aula34-for-estrutura-de-repeticao/for.js
+++ aula34-for-estrutura-de-repeticao/for.js
@@ -10,7 +10,7 @@
 // 1. Criar uma variável de controle;
 // 2. Condição, para dizer quando esse for parar;
 // 3. Incrementa para atingir a condição
-for (let i = 500; i >= 400; i--) {  // Da pra brincar com os sinais
+for (let i = 0; i <= 10; i += 2) {  // Da pra brincar com os sinais
   console.log(`${i}`);
 }
 
@@ -19,3 +19,17 @@ Começar do -100 -> let i = -100
 Pular de 10 em 10 -> i += 10
 1 em 1 -> i++
 */
+
+
+for (let i = 0; i <= 10; i++) {
+  const par = i % 2 === 0 ? 'par' : 'ímpar';  // Checa se o número é par ou impar com operação ternária
+  console.log(i, par);
+}
+
+
+// Índices        0       1       2
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Menlancia', 'Laranja', 'Morango', 'Banana'];  // 3 Elementos
+
+for (let i = 0; i < frutas.length; i++) {  // Não para até mostrar todos os elementos ou indices de um array
+  console.log(`Índice ${i} - ${frutas[i]}`);
+}
