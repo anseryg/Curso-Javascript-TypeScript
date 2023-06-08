7f377134a26716942856bdc0aa3e1674972f8297
dreynus <72932055+dreynus@users.noreply.github.com>
2023-06-08 17:43:16 -0300

    for

---
+++ aula34-for-estrutura-de-repeticao/for.js
@@ -0,0 +1,21 @@
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+// for
+// Passar 3 coisas
+// Criar uma variável, criar uma condição que vai parar  o laço, e incrementar ou decrementar a variável. i - index.
+// 1. Criar uma variável de controle;
+// 2. Condição, para dizer quando esse for parar;
+// 3. Incrementa para atingir a condição
+for (let i = 500; i >= 400; i--) {  // Da pra brincar com os sinais
+  console.log(`${i}`);
+}
+
+/* Da pra brincar com os sinais. Exemplos:
+Começar do -100 -> let i = -100
+Pular de 10 em 10 -> i += 10
+1 em 1 -> i++
+*/
